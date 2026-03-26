6b37caae1740cc800033a1e58273adf5ae49cc57
Darya Homenok <daria.khomenok12317@gmail.com>
2026-03-26 11:00:41 +0700

    исправление

--- main/views.py
+++ main/views.py
@@ -42,6 +42,10 @@ def tracks(request):
     t = Track.objects.all()
     return render(request, 'tracks.html', {'tracks': t})
 
+<<<<<<< HEAD
+=======
+#исполнители
+>>>>>>> work
 def artists(request):
     a = Artist.objects.all()
     return render(request, 'artists.html', {'artists': a})
